f97664b351474e4eebd3233cd5301ba81b634726
Alex Logvin <alexlogvin.2013@gmail.com>
2020-12-13 15:34:08 +0100

    initial realease

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,13 +29,117 @@ namespace Easy_Photo_Remover
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.imageViewer = new System.Windows.Forms.PictureBox();
+            this.btnFolderChooser = new System.Windows.Forms.Button();
+            this.lblChoosenFolder = new System.Windows.Forms.Label();
+            this.folderChooser = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblTotalImages = new System.Windows.Forms.Label();
+            this.lblImagesToDelete = new System.Windows.Forms.Label();
+            this.lblAction = new System.Windows.Forms.Label();
+            this.btnDeleteImages = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageViewer
+            // 
+            this.imageViewer.Location = new System.Drawing.Point(12, 52);
+            this.imageViewer.Name = "imageViewer";
+            this.imageViewer.Size = new System.Drawing.Size(1234, 1139);
+            this.imageViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageViewer.TabIndex = 0;
+            this.imageViewer.TabStop = false;
+            // 
+            // btnFolderChooser
+            // 
+            this.btnFolderChooser.Location = new System.Drawing.Point(12, 12);
+            this.btnFolderChooser.Name = "btnFolderChooser";
+            this.btnFolderChooser.Size = new System.Drawing.Size(154, 34);
+            this.btnFolderChooser.TabIndex = 1;
+            this.btnFolderChooser.Text = "Chose folder...";
+            this.btnFolderChooser.UseVisualStyleBackColor = true;
+            this.btnFolderChooser.Click += new System.EventHandler(this.btnFolderChooser_Click);
+            // 
+            // lblChoosenFolder
+            // 
+            this.lblChoosenFolder.AutoSize = true;
+            this.lblChoosenFolder.Location = new System.Drawing.Point(172, 17);
+            this.lblChoosenFolder.Name = "lblChoosenFolder";
+            this.lblChoosenFolder.Size = new System.Drawing.Size(166, 25);
+            this.lblChoosenFolder.TabIndex = 2;
+            this.lblChoosenFolder.Text = "Folder not choosen";
+            // 
+            // lblTotalImages
+            // 
+            this.lblTotalImages.AutoSize = true;
+            this.lblTotalImages.Location = new System.Drawing.Point(12, 1194);
+            this.lblTotalImages.Name = "lblTotalImages";
+            this.lblTotalImages.Size = new System.Drawing.Size(130, 25);
+            this.lblTotalImages.TabIndex = 3;
+            this.lblTotalImages.Text = "Total images: 0";
+            // 
+            // lblImagesToDelete
+            // 
+            this.lblImagesToDelete.AutoSize = true;
+            this.lblImagesToDelete.Location = new System.Drawing.Point(13, 1223);
+            this.lblImagesToDelete.Name = "lblImagesToDelete";
+            this.lblImagesToDelete.Size = new System.Drawing.Size(164, 25);
+            this.lblImagesToDelete.TabIndex = 4;
+            this.lblImagesToDelete.Text = "Images to delete: 0";
+            // 
+            // lblAction
+            // 
+            this.lblAction.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAction.AutoSize = true;
+            this.lblAction.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAction.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblAction.Location = new System.Drawing.Point(1118, 69);
+            this.lblAction.Name = "lblAction";
+            this.lblAction.Size = new System.Drawing.Size(112, 45);
+            this.lblAction.TabIndex = 5;
+            this.lblAction.Text = "Action";
+            this.lblAction.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnDeleteImages
+            // 
+            this.btnDeleteImages.Location = new System.Drawing.Point(1017, 1197);
+            this.btnDeleteImages.Name = "btnDeleteImages";
+            this.btnDeleteImages.Size = new System.Drawing.Size(229, 52);
+            this.btnDeleteImages.TabIndex = 6;
+            this.btnDeleteImages.Text = "Delete marked images";
+            this.btnDeleteImages.UseVisualStyleBackColor = true;
+            this.btnDeleteImages.Click += new System.EventHandler(this.btnDeleteImages_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1258, 1261);
+            this.Controls.Add(this.btnDeleteImages);
+            this.Controls.Add(this.lblAction);
+            this.Controls.Add(this.lblImagesToDelete);
+            this.Controls.Add(this.lblTotalImages);
+            this.Controls.Add(this.lblChoosenFolder);
+            this.Controls.Add(this.btnFolderChooser);
+            this.Controls.Add(this.imageViewer);
+            this.KeyPreview = true;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imageViewer;
+        private System.Windows.Forms.Button btnFolderChooser;
+        private System.Windows.Forms.Label lblChoosenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderChooser;
+        private System.Windows.Forms.Label lblTotalImages;
+        private System.Windows.Forms.Label lblImagesToDelete;
+        private System.Windows.Forms.Label lblAction;
+        private System.Windows.Forms.Button btnDeleteImages;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,21 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Easy_Photo_Remover
 {
     public partial class MainForm : Form
     {
+        class ImageInfo
+        {
+            public string Path;
+            public bool ToDelete = false;
+        }
+
+        private string SourceFolder;
+
+        private ImageInfo CurrentImage = null;
+        private int CurrentImageIndex
+        {
+            get => _currentImageIndex; set
+            {
+                if (value < 0)
+                    _currentImageIndex = ImagesCount - 1;
+                else if (value >= ImagesCount)
+                    _currentImageIndex = 0;
+                else _currentImageIndex = value;
+                CurrentImage = Images.ElementAtOrDefault(_currentImageIndex);
+                UpdateImageViewer();
+            }
+        }
+        private int _currentImageIndex = 0;
+
+        private List<ImageInfo> Images
+        {
+            get => _images; set
+            {
+                _images = value;
+                ImagesCount = _images.Count();
+                ImagesToDeleteCount = 0;
+                lblTotalImages.Text = $"Total images: {ImagesCount}";
+                if (ImagesCount > 0)
+                    CurrentImageIndex = 0;
+            }
+        }
+        private List<ImageInfo> _images = new List<ImageInfo>();
+        private int ImagesCount = 0;
+        private int ImagesToDeleteCount
+        {
+            get => _imagesToDeleteCount; set
+            {
+                _imagesToDeleteCount = value;
+                lblImagesToDelete.Text = $"Images to delete: {ImagesToDeleteCount}";
+            }
+        }
+        private int _imagesToDeleteCount = 0;
+
+        private readonly List<string> AllowedExtentions = new List<string> { ".jpg", ".png", ".jpeg" };
+
         public MainForm()
         {
             InitializeComponent();
         }
 
+        private void btnFolderChooser_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderChooser.ShowDialog();
+            if (result == DialogResult.OK)
+                UpdateImagesList(folderChooser.SelectedPath);
+        }
+
+        private void UpdateImagesList(string folderPath)
+        {
+            lblChoosenFolder.Text = SourceFolder = folderPath;
+            Images = Directory.GetFiles(SourceFolder).Where(x => AllowedExtentions.Contains(new FileInfo(x).Extension)).Select(x => new ImageInfo { Path = x }).ToList();
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (ImagesCount > 0)
+            {
+                if (e.KeyCode == Keys.Delete)
+                {
+                    ImageInfo image = Images[CurrentImageIndex];
+                    image.ToDelete = !image.ToDelete;
+                    ImagesToDeleteCount += image.ToDelete ? 1 : -1;
+
+                    if (image.ToDelete)
+                        CurrentImageIndex++;
+                    UpdateImageStatus();
+                }
+
+                else if (e.KeyCode == Keys.Left)
+                    CurrentImageIndex--;
+                else if (e.KeyCode == Keys.Right)
+                    CurrentImageIndex++;
+            }
+            imageViewer.Focus();
+        }
+
+        private void UpdateImageStatus()
+        {
+            lblAction.Text = CurrentImage.ToDelete ? "Delete" : "Save";
+            lblAction.BackColor = CurrentImage.ToDelete ? Color.Red : Color.Green;
+        }
+
+        private void UpdateImageViewer()
+        {
+            imageViewer.Image?.Dispose();
+
+            if (CurrentImage == null)
+                return;
+
+            imageViewer.Image = Image.FromFile(CurrentImage.Path);
+
+            //If image is larger than picturebox
+            if (imageViewer.Image.Width > imageViewer.ClientSize.Width || imageViewer.Image.Height > imageViewer.ClientSize.Height)
+                imageViewer.SizeMode = PictureBoxSizeMode.Zoom;
+
+            //If image is smaller than picturebox
+            else
+                imageViewer.SizeMode = PictureBoxSizeMode.CenterImage;
+
+            UpdateImageStatus();
+        }
+
+        private void btnDeleteImages_Click(object sender, EventArgs e)
+        {
+            if (ImagesCount > 0)
+            {
+                imageViewer.Image?.Dispose();
+
+                foreach (ImageInfo image in Images)
+                    if (image.ToDelete)
+                        if (new FileInfo(image.Path).Exists)
+                            File.Delete(image.Path);
+
+                UpdateImagesList(SourceFolder);
+            }
+        }
     }
 }
